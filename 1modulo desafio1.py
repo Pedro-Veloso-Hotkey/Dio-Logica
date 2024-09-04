b2011b427df2f79ce8ecc185ed2769a7b2600815
Pedro-Veloso-Hotkey <pedrinytp@gmail.com>
2024-09-04 16:05:37 -0300

    Delete 1modulo desafio1.py

--- 1modulo desafio1.py
+++
@@ -1,8 +0,0 @@
-# Solicita ao usuário um número inteiro
-numero = int(input())
-
-# TODO: Verifique se o número é par ou ímpar e imprima o resultado:
-if numero % 2 == 0:
-    print("Este número é par")
-else: 
-    print("Este numero é impar")
