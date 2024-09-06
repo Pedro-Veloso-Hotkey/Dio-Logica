6c2d467abe8243b38823ce11e8d0ab4189ea7270
Pedro-Veloso-Hotkey <pedrinytp@gmail.com>
2024-09-05 22:12:05 -0300

    atualizando

--- CursoLogica.py
+++ CursoLogica.py
@@ -3,21 +3,21 @@
 nome = "Cegolas"          # ou input para interação com usuário    
 experiencia = int("7000")  # ou input 
 
-if experiencia < 1001:
+if experiencia < 1000:
         nivel = "Ferro" 
-elif 1001 <= experiencia < 2001:
+elif 1000 <= experiencia < 2000:
         nivel = "Bronze"
-elif 2001 <= experiencia < 5001:
+elif 2000 <= experiencia < 5000:
         nivel = "Prata"
-elif 5001 <= experiencia < 7001:
+elif 5000 <= experiencia < 7000:
         nivel = "Ouro"
-elif 7001 <= experiencia < 8001:
+elif 7000 <= experiencia < 8000:
         nivel = "Platina"
-elif 8001 <= experiencia < 9001:
+elif 8000 <= experiencia < 9000:
         nivel = "Ascendente"
-elif 9001 <= experiencia < 10001:
+elif 9000 <= experiencia < 10000:
         nivel = "Imortal"
-elif 10001 <= experiencia:
+elif 10000 <= experiencia:
         nivel = "Radiante"
     
 print(f"O Herói de nome {nome} está no nível de {nivel}")
